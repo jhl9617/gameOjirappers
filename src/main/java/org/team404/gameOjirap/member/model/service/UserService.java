789c60f0069b68aa07a71d7f2595393b0480004f
okjung123 <ttwmrk2002@naver.com>
2023-03-27 13:11:11 +0900

    수정주석달았음 (테스트용) g

--- src/main/java/org/team404/gameOjirap/member/model/service/UserService.java
+++ src/main/java/org/team404/gameOjirap/member/model/service/UserService.java
@@ -1,14 +1,14 @@
 package org.team404.gameOjirap.member.model.service;
 
-import org.team404.gameOjirap.member.model.vo.Member;
+import org.team404.gameOjirap.member.model.vo.User;
 
 import java.util.ArrayList;
 
 //스프링에서는 모델의 서비스 클래스는 인터페이스로 만들도록 정해져 있음
-public interface MemberService {
-	Member selectLogin(Member member);
-	ArrayList<Member> selectList();
-	int insertMember(Member member);
-	int updateMember(Member member);
+public interface UserService {
+	User selectLogin(User member);
+	ArrayList<User> selectList();
+	int insertMember(User member);
+	int updateMember(User member);
 	int deleteMember(String userid);	
 }
--- src/main/java/org/team404/gameOjirap/member/model/service/UserServiceImpl.java
+++ src/main/java/org/team404/gameOjirap/member/model/service/UserServiceImpl.java
@@ -2,39 +2,39 @@ package org.team404.gameOjirap.member.model.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.team404.gameOjirap.member.model.dao.MemberDao;
-import org.team404.gameOjirap.member.model.vo.Member;
+import org.team404.gameOjirap.member.model.dao.UserDao;
+import org.team404.gameOjirap.member.model.vo.User;
 
 import java.util.ArrayList;
 
 //스프링에서는 서비스 모델 클래스는 서비스 인터페이스를 상속받아서
 //만들도록 되어 있음
 @Service("memberService")  //xml 자동 등록 처리됨
-public class MemberServiceImpl implements MemberService {
+public class UserServiceImpl implements UserService {
 
 	//DAO 와 연결 처리
 	@Autowired  //자동 DI 처리됨 : 자동 객체 생성됨
-	MemberDao memberDao;	
+	UserDao memberDao;	
 	
 	@Override
-	public Member selectLogin(Member member) {
+	public User selectLogin(User member) {
 		return memberDao.selectLogin(member);
 	}
 
 	@Override
-	public ArrayList<Member> selectList() {
+	public ArrayList<User> selectList() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public int insertMember(Member member) {
+	public int insertMember(User member) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public int updateMember(Member member) {
+	public int updateMember(User member) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
