66aae015a4f266f75b3acc8446c6a0caff13ed51
LeeHyunBae <128680730+LeeHyunBae@users.noreply.github.com>
2023-03-27 23:16:37 +0900

    Board 패키지 내용 추가
    Board & BoardService & BoardServiceImpl & BoardDao 코드 작성

--- src/main/java/org/team404/gameOjirap/board/model/dao/BoardDao.java
+++ src/main/java/org/team404/gameOjirap/board/model/dao/BoardDao.java
@@ -1,5 +1,60 @@
 package org.team404.gameOjirap.board.model.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.team404.gameOjirap.board.model.vo.Board;
+
+@Repository("boardDao")
 public class BoardDao {
+
+	@Autowired
+	private SqlSessionTemplate session;
+	
+	public ArrayList<Board> selectList() {
+		List<Board> list = session.selectList("boardMapper.selectList");
+		return (ArrayList<Board>)list;
+	}
+
+	public Board selectOne(int board_no) {
+		return session.selectOne("boardMapper.selectOne", board_no);
+	}
+
+	public ArrayList<Board> selectBoardByTitle(String keyword) {
+		List<Board> list = session.selectList("boardMapper.selectBoardByTitle", keyword);
+		return (ArrayList<Board>)list;
+	}
+
+	public int selectListCount() {
+		return session.selectOne("boardMapper.selectListCount");
+	}
+
+	public int insertBoard(Board board) {
+		return session.insert("boardMapper.insertBoard", board);
+	}
+
+	public int updateBoard(Board board) {
+		return session.update("boardMapper.updateBoard", board);
+	}
+
+	public int deleteBoard(Board board) {
+		return session.delete("boardMapper.deleteBoard", board);
+	}
+
+	public ArrayList<Board> selectRankTop5() {
+		List<Board> list = session.selectList("boardMapper.selectRankTop5");
+		return (ArrayList<Board>)list;
+	}
+
+	public int insertBoardLike(int board_no) {
+		return session.insert("boardMapper.insertBoardLike", board_no);
+	}
+
+	public int deleteBoardLike(int board_no) {
+		return session.delete("boardMapper.deleteBoardLike", board_no);
+	}
 
 }
--- src/main/java/org/team404/gameOjirap/board/model/service/BoardServiceImpl.java
+++ src/main/java/org/team404/gameOjirap/board/model/service/BoardServiceImpl.java
@@ -1,5 +1,64 @@
 package org.team404.gameOjirap.board.model.service;
 
-public class BoardServiceImpl {
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.team404.gameOjirap.board.model.dao.BoardDao;
+import org.team404.gameOjirap.board.model.vo.Board;
+
+public class BoardServiceImpl implements BoardService {
+
+	@Autowired
+	private BoardDao boardDao;
+	
+	@Override
+	public ArrayList<Board> selectList() {
+		return boardDao.selectList();
+	}
+
+	@Override
+	public Board selectOne(int board_no) {
+		return boardDao.selectOne(board_no);
+	}
+
+	@Override
+	public ArrayList<Board> selectBoardByTitle(String keyword) {
+		return boardDao.selectBoardByTitle(keyword);
+	}
+
+	@Override
+	public int selectListCount() {
+		return boardDao.selectListCount();
+	}
+
+	@Override
+	public int insertBoard(Board board) {
+		return boardDao.insertBoard(board);
+	}
+
+	@Override
+	public int updateBoard(Board board) {
+		return boardDao.updateBoard(board);
+	}
+
+	@Override
+	public int deleteBoard(Board board) {
+		return boardDao.deleteBoard(board);
+	}
+
+	@Override
+	public ArrayList<Board> selectRankTop5() {
+		return boardDao.selectRankTop5();
+	}
+
+	@Override
+	public int insertBoardLike(int board_no) {
+		return boardDao.insertBoardLike(board_no);
+	}
+
+	@Override
+	public int deleteBoardLike(int board_no) {
+		return boardDao.deleteBoardLike(board_no);
+	}
 
 }
