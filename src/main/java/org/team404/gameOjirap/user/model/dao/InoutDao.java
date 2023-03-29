e89fa1f8b58b8f0c00d8c60c23585388cc76c6e2
okjung123 <ttwmrk2002@naver.com>
2023-03-29 16:13:46 +0900

    유저쪽 java 파일 생성

---
+++ src/main/java/org/team404/gameOjirap/user/model/dao/InoutDao.java
@@ -0,0 +1,82 @@
+package org.team404.gameOjirap.user.model.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.team404.gameOjirap.user.model.vo.MyPage;
+import org.team404.gameOjirap.user.model.vo.User;
+
+@Repository("InoutDao")
+public class InoutDao {
+	//★★★★★★MemberServiceImpl로부터 받은 값을 처리해서 Controller로 넘기는 파트임!
+
+	//마이바티스 연결
+	@Autowired		//@Autowired를 통해 의존성(DI)주입됨 => root-context.xml 에서 생성한 객체와 자동 연결됨=> mapper, config, DB까지 모두 연결됨
+	private SqlSessionTemplate session;	
+
+
+	public User loginMethod(User user) {
+		return session.selectOne("userMapper.loginMethod", user);
+	}
+	
+	public ArrayList<User> selectList() {
+		List<User> list = session.selectList("userMapper.selectList");	
+		return (ArrayList<User>)list;
+	}
+	
+	public User selectUser(String user_id) {
+		return session.selectOne("userMapper.selectUser", user_id);	
+	}
+
+	
+	public int selectDupCheckId(String user_id) {
+		return session.selectOne("userMapper.selectDupCheckId", user_id);	
+	}
+	
+	public int selectDupCheckNick(String user_nickname) {
+		return session.selectOne("userMapper.selectDupCheckId", user_nickname);	
+	}
+
+	public int userDeleteMethod(String user_id) {
+		return session.delete("deleteUser", user_id);
+	}
+
+
+	public int userInsertMethod(User user) {
+		return session.insert("userMapper.insertMember", user);
+	}
+
+	public int levelMethod(MyPage user_level) {
+		return session.insert("userMapper.levelMethod", user_level);
+	}
+
+	public int checkanswer(User confirm_answer) {
+		return session.insert("userMapper.checkanswer", confirm_answer);
+	}
+
+
+	public int checkanswer(String confirm_answer) {
+		return session.insert("userMapper.checkanswer", confirm_answer);
+	}
+
+	public int updateLoginok(User user) {
+		return session.update("userMapper.updateLoginok", user);
+	}
+
+
+
+}//class close
+
+
+
+
+
+
+
+
+
+
+
