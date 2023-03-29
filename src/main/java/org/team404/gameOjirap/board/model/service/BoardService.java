c4b1488954b1c22457581504867bd45c425d2f08
LeeHyunBae <128680730+LeeHyunBae@users.noreply.github.com>
2023-03-29 23:58:10 +0900

    board 패키지 내용 추가
    updateBoardReadCount 메소드 추가
    mybatis-config에 typeAlias 추가common패키지에 paging 클래스 추가
    board-mapper selectList 내용 추가

--- src/main/java/org/team404/gameOjirap/board/model/service/BoardService.java
+++ src/main/java/org/team404/gameOjirap/board/model/service/BoardService.java
@@ -3,10 +3,11 @@ package org.team404.gameOjirap.board.model.service;
 import java.util.ArrayList;
 
 import org.team404.gameOjirap.board.model.vo.BoardGen;
+import org.team404.gameOjirap.common.Paging;
 
 public interface BoardService {
-	ArrayList<BoardGen> selectList();	//게시글 전체 리스트 보기용
-	BoardGen selectOne(int board_no);	//해당 게시글 번호에 대한 조회수 1증가 처리용
+	ArrayList<BoardGen> selectList(Paging page); //한 페이지에 보여질 게시글 조회용
+	BoardGen selectOne(int board_no);	//해당 게시글 번호에 대한 게시글 상세 조회용
 	ArrayList<BoardGen> selectBoardByTitle(String keyword); //게시글 제목으로 검색용
 	int selectListCount();  //총 게시글 갯수 조회용 (페이지 수 계산용)
 	int insertBoard(BoardGen board);  //게시글 등록용
@@ -15,5 +16,6 @@ public interface BoardService {
 	ArrayList<BoardGen> selectRankTop5();  //조회수 상위 5개 게시물 조회용
 	int insertBoardLike(int board_no);	//좋아요 수 증가 처리용
 	int deleteBoardLike(int board_no);  //좋아요 취소( -1 ) 처리용
+	int updateBoardReadCount(int board_no);  //게시글 상세보기시 조회수 1증가 처리용
 	
 }
