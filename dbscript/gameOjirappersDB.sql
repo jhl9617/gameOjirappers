d55cbc0beca2426fbb9475f627a0663420946907
JonghaLee <jhl9617@gmail.com>
2023-04-12 12:04:56 +0900

    tb_Genboard_like 추가

--- dbscript/gameOjirappersDB.sql
+++ dbscript/gameOjirappersDB.sql
@@ -12,6 +12,20 @@ comment on column tb_board_like.user_id is '좋아요누른회원';
 comment on column tb_board_like.board_no is '좋아요누른게시물';
 comment on column tb_board_like.like_date is '좋아요누른날짜';
 
+drop table tb_board_like cascade constraints;
+
+create table tb_Genboard_like(
+                              user_id VARCHAR2(50 BYTE) ,
+                              board_no number not null,
+                              like_date date not null,
+                              constraint pk_genboard_like primary key (user_id, board_no),
+                              constraint fk_genboard_like foreign key (user_id) references tb_user(user_id)
+);
+
+comment on column tb_Genboard_like.user_id is '좋아요누른회원';
+comment on column tb_Genboard_like.board_no is '좋아요누른게시물';
+comment on column tb_Genboard_like.like_date is '좋아요누른날짜';
+
 DROP TABLE TB_USER cascade constraints;
 -- 소셜로그인시 회원가입을 이용할 경우의 회원 테이블
 CREATE TABLE TB_USER (
@@ -461,6 +475,8 @@ CREATE TABLE TB_RECORD (
 );
 comment on column TB_RECORD.user_id IS '사용자 식별자';
 comment on column TB_RECORD.login_date IS '로그인 날짜';
+
+
 
 ALTER TABLE TB_USER
     ADD CONSTRAINT PK_TB_USER
